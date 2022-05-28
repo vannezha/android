aea9154120641e1c9c84d2c6993171843777128d
Vanny Ezhaan Nur Sandika <vannyezhaa@gmail.com>
2022-05-28 13:50:29 +0700

    add several task

--- kotlin-starter-with-IDE/src/main/kotlin/com/vanny/kotlin/App.kt
+++ kotlin-starter-with-IDE/src/main/kotlin/com/vanny/kotlin/App.kt
@@ -1,5 +1,31 @@
 package com.vanny.kotlin
 
+// single line comment
+
+/*
+multiline
+comment
+*/
+
 fun main() {
+//    task 1
     println("Hello kotlin")
+
+//    task 2
+    val name = "Vanny"
+    print("Hello my name is ")
+    println(name)
+    println(if (true) "always uwun't" else "always uwu")
+
+//    task 3 (var and val)
+/*
+*  we can change variable value if we initiate it
+* with var keyword,
+* otherwise val cant
+*/
+    var nama = "Vanny uwu"
+    nama = "uwu Vanny"
+//    name = "uwu"  --> if we run this will throw an
+//    error because we have initiated variable "name" with val keyword
+//    nb : val must be inititated its value
 }
