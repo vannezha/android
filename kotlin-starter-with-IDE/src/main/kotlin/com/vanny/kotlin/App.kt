f6463b36b3cd838c91fa659fd53280fedca86546
Vanny Ezhaan Nur Sandika <vannyezhaa@gmail.com>
2022-05-28 14:03:25 +0700

    data type and variable structure

--- kotlin-starter-with-IDE/src/main/kotlin/com/vanny/kotlin/App.kt
+++ kotlin-starter-with-IDE/src/main/kotlin/com/vanny/kotlin/App.kt
@@ -27,5 +27,22 @@ fun main() {
     nama = "uwu Vanny"
 //    name = "uwu"  --> if we run this will throw an
 //    error because we have initiated variable "name" with val keyword
-//    nb : val must be inititated its value
+//    nb : val must be inititated its value before
+
+//    task 4 (variable structure)
+    var jeneng : String = "budi sans"
+//    var/val = controll value
+//    jeneng = identifier
+//    String = type of variable we want intiate (we can ignore it since kotlin static type)
+//    "budi sans" = initialization
+
+//    task 5 (data type determine its operation that can be used)
+    val kata1 :String= "aku suka"
+    val kata2 = "kucing yang"
+    val kata3 = "ga garong"
+    println(kata1 + " " + kata2 + " " +kata3)
+// it will give us different result if our variable not string like this
+    val angka1 :Int = 12
+    val angka2 = 4.0
+    println(angka1 + angka2)
 }
